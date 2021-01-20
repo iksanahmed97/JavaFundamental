bb18f1ad6629158e1f435bbdd1e515f127d267ac
iksanahmed97 <iksanpty40@gmail.com>
2021-01-20 15:11:26 +0700

    first commit

---
+++ ContohArrow.js
@@ -0,0 +1,29 @@
+
+
+//nilai merupakan fungsi hitungkali
+//hitungkali akan mereturn fungsi test
+/*
+let nilai = function HitungKali (a.b){
+    return function Test (){
+        return "Akhirnya ketemu saya" + a + b
+    }
+}
+*/
+//arrow function
+let nilai = (a,b) => (x) => {
+    return "akhirnya ketemu saya" + a + b+x;
+}
+
+//jalani sekarang karena mengexecute fungsi nilai dia menjadi fungsi Test
+let jalan1 = nilai(2,3);
+
+//karena jalan1 sama dengan fungsi Test maka dia akan mereturn string
+console.log(jalan1());
+
+//atau jika ingin mempersingkat tanpa harus membuat variable baru untuk mendapatkan
+//fungsi Test() dapat diketikan seperti ini
+console.log(nilai(2,3)(5));
+
+
+
+
